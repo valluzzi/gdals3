90b6ede1e1192d9ddf47328b7924586192f0c06a
valluzzi <valluzzi@gmail.com>
2023-09-25 16:19:33 +0200

    fix

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
+set PAT="your github token"
 set name=gdals3
 docker build -t %name% .
-::docker build --build-arg ACCESS_KEY=your-access-key --build-arg SECRET_KEY=your-secret-key -t %name% .
 echo %PAT% | docker login ghcr.io --username valluzzi --password-stdin
 docker tag %name% ghcr.io/valluzzi/%name%:latest
 docker push ghcr.io/valluzzi/%name%:latest
